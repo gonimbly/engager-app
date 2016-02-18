9b2bf1bfc0c8275d2e5287d6992243f06a21cbe0
Grant H <ghelton@gmail.com>
2016-02-18 15:53:49 -0800

    analytics added support for ejs views and added analytics

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,8 @@ var _create = require('lodash.create');
 var webpackConfig = require('./webpack.config.js');
 var files = {
   main: './app/main.js',
-  app: 'app/**/*.*',
-  index: 'app/index.html'
+  app: 'app/**/*.*'
 }
-
-// ***
-// Build index file
-// ***
-gulp.task('build-index', function() {
-  return gulp.src(files.index)
-    .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('watch-index', ['build-index'], function() {
-  return gulp.watch(files.index, ['build-index']);
-});
 
 // ***
 // Build local files
@@ -48,7 +35,7 @@ gulp.task('watch-scripts', ['build-scripts'], function() {
 // Main tasks
 // ***
 
-gulp.task('serve', ['watch-index', 'watch-scripts'], function() {
+gulp.task('serve', ['watch-scripts'], function() {
   return nodemon({
     script: 'server/server.js',
     env: {

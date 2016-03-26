19e0f37cd805eb8c5342ccd0ac2af1a03117cc46
Troy Conquer <troy@gonimbly.com>
2016-03-25 19:08:06 -0700

    fixing query, promises and catch block ..

--- bin/migrations/20160310144119_emoji.js
+++ bin/migrations/20160310144119_emoji.js
@@ -1,11 +1,31 @@
 'use strict';
 
+var Answer = require('../../db/models/answer');
+var Bromise = require('bluebird');
+
 exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.table('answers', function(table) {
-      table.text('emoji');
+
+  return Bromise.resolve()
+    .then(function(){
+      // return knex.schema.table('answers', function(table) {
+      //     table.text('emoji');
+      //   });
+      console.log('Answer,',Answer);
+      console.log('Answer.forge,',Answer.forge);
+      return Answer
+        .forge()
+        .fetchAll({require: true, debug:true})
+        .then(function(models){
+          console.log('models',models);
+        })
+        .catch(function(bad){
+          console.log('bad',bad);
+        })
     })
-  ]);
+    .then(function(obj){
+      console.log('obj',obj);
+      throw 'Wont finish migration';
+    });
 };
 
 exports.down = function(knex, Promise) {
